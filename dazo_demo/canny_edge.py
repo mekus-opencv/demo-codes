54c5ad3847f7413cef6c44e438554b5daa59fe8e
olazo-johnalbert <jaolazo4@gmail.com>
2025-07-12 20:37:05 +0800

    add line to show the original image

--- dazo_demo/canny_edge.py
+++ dazo_demo/canny_edge.py
@@ -5,6 +5,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 canny_edges = cv2.Canny(gray, 100, 200)
 
+cv2.imshow("Original Image", img)
 cv2.imshow("Canny Edge Detection", canny_edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
