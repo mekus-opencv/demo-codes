7490a6cd531bc12976cceb3de07d3a967c4e7318
olazo-johnalbert <jaolazo4@gmail.com>
2025-07-12 20:37:34 +0800

    add line to show the original image

--- dazo_demo/gaussian_blur.py
+++ dazo_demo/gaussian_blur.py
@@ -4,6 +4,7 @@ img = cv2.imread("image.png")
 
 gaussian_blur = cv2.GaussianBlur(img, (9, 9), 0)
 
+cv2.imshow("Original Image", img)
 cv2.imshow("Gaussian Blur", gaussian_blur)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
