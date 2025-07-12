a13b884968ac9ab08182dcad008b02e7e17473f2
olazo-johnalbert <jaolazo4@gmail.com>
2025-07-12 20:32:53 +0800

    modify conversion code to bgr->gray conversion

--- agulto_demo/color_conversion.py
+++ agulto_demo/color_conversion.py
@@ -1,8 +1,8 @@
 import cv2
 
 image = cv2.imread("image.png")
-converted_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+converted_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Output Image", converted_image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()s
+cv2.destroyAllWindows()
