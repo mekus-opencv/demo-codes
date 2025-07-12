20473e90c7a9f008ebfa2c5781d707cbf6d39e59
olazo-johnalbert <jaolazo4@gmail.com>
2025-07-12 20:38:30 +0800

    add line to show the original image

--- dazo_demo/laplacian_edge.py
+++ dazo_demo/laplacian_edge.py
@@ -6,6 +6,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 laplacian = cv2.Laplacian(gray, cv2.CV_64F, ksize=3)
 laplacian = cv2.convertScaleAbs(laplacian)
 
+cv2.imshow("Original Image", img)
 cv2.imshow("Laplacian Edge Detection", laplacian)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
