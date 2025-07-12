0613595b4402b2a3e3c997c0905a6dbfa8b0f88c
olazo-johnalbert <jaolazo4@gmail.com>
2025-07-12 20:39:17 +0800

    remove image resize line

--- dazo_demo/sharpen.py
+++ dazo_demo/sharpen.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 img = cv2.imread("image.png")
-img = cv2.resize(img, (600, 400))
 
 sharpen_kernel = np.array([
     [0, -1, 0],
@@ -12,6 +11,7 @@ sharpen_kernel = np.array([
 
 sharpened = cv2.filter2D(img, -1, sharpen_kernel)
 
+cv2.imshow("Original Image", img)
 cv2.imshow("Sharpened", sharpened)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
